dfc9e7c219480e1df2e665f2aa3cbb39906d2816
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-07-21 12:43:14 +0300

    inline editing feature

---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="task-actions">
+        <button class="btn btn-sm btn-circle btn-outline-secondary me-1" @click="emit('edit')">
+            <PencilIcon />
+        </button>
+        <button class="btn btn-sm btn-circle btn-outline-danger" @click="emit('remove')">
+            <TrashIcon />
+        </button>
+    </div>
+</template>
+
+<script setup>
+import PencilIcon from '../icons/PencilIcon.vue'
+import TrashIcon from '../icons/TrashIcon.vue'
+
+const emit = defineEmits(['edit', 'remove'])
+</script>
